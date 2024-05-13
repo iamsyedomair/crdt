bfa87a94dd4e53dd9e3a44e766e3eed77e8595f8
sn0wcr@sh <syedomair0@gmail.com>
2024-05-12 22:04:50 -0500

    simple caching node

---
+++ CacheNode.py
@@ -0,0 +1,23 @@
+import uuid
+
+class CacheNode:
+    def __init__(self):
+        self.node_id = str(uuid.uuid4())
+        self.cache = {}
+
+    def get(self, key):
+        return self.cache.get(key)
+
+    def put(self, key, value):
+        self.cache[key] = value
+        self.increment_counter(key)
+
+    def increment_counter(self, key):
+        counter = self.cache.get(key + '_counter', GCounter())
+        counter.increment(self.node_id)
+        self.cache[key + '_counter'] = counter
+
+    def merge_counters(self, key, other_counter):
+        counter = self.cache.get(key + '_counter', GCounter())
+        counter.merge(other_counter)
+        self.cache[key + '_counter'] = counter
